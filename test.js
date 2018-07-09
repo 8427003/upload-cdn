6c2aef15c07051297b1aa8f28557f81c329f1647
lijun <lijun401338@qq.com>
2018-07-09 16:53:09 +0800

    trans group

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const uploadCDN = require('./index.js');
 const SSO_CONFIG = {
     region: 'oss-cn-beijing',
     accessKeyId: 'LTAI3aUUyVOY5q06',
-    accessKeySecret: 'WZDibtiE0nwOCjHBwWMYGX0jUtz2de',
+    accessKeySecret: 'WZDibtiE0nwOCjHBwWMYGX0jUtz2d',
     bucket: 'css3-img'
 }
 
 uploadCDN(SSO_CONFIG, {
-    namespace: 'static-pages',
+    namespace: 'static-pages/static',
     uploadDir: './test-x'
 })
